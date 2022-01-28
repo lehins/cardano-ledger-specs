7b8d49fc92d712d7e22d4ed0524abcd7c91375a1
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2022-01-28 16:21:03 +0300

    Improve TxOut benchmarks

--- libs/cardano-ledger-test/bench/Bench/Cardano/Ledger/TxOut.hs
+++ libs/cardano-ledger-test/bench/Bench/Cardano/Ledger/TxOut.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Bench.Cardano.Ledger.TxOut (benchTxOut) where
 
@@ -22,6 +23,8 @@ import Control.DeepSeq
 import Criterion.Main
 import Data.Map.Strict (singleton)
 import Data.Maybe
+import qualified Data.Text as T
+import GHC.Records
 
 type A = AlonzoEra StandardCrypto
 
@@ -29,50 +32,63 @@ benchTxOut :: Benchmark
 benchTxOut =
   let ada :: Value StandardCrypto
       ada = inject (Coin 100)
-      key :: Credential 'Payment StandardCrypto
-      key = KeyHashObj payAddr28
+      key :: Int -> Credential 'Payment StandardCrypto
+      key = KeyHashObj . payAddr28
       stake :: StakeReference StandardCrypto
       stake = StakeRefBase (KeyHashObj stakeAddr28)
-      addr :: Addr StandardCrypto
-      addr = Addr Mainnet key stake
+      addr :: Int -> Addr StandardCrypto
+      addr n = Addr Mainnet (key n) stake
       value :: Value StandardCrypto
       value = Value 200 (singleton (PolicyID policyId28) (singleton assName 217))
-      txOutAddr :: TxOut A
-      txOutAddr = TxOut addr value SNothing
-      txOutAddrAdaOnly :: TxOut A
-      txOutAddrAdaOnly = TxOut addr ada SNothing
-      txOutAddrAdaOnlyDataHash :: TxOut A
-      txOutAddrAdaOnlyDataHash = TxOut addr ada (SJust dataHash32)
+      txOutAddr :: Int -> TxOut A
+      txOutAddr n = TxOut (addr n) value SNothing
+      txOutAddrAdaOnly :: Int -> TxOut A
+      txOutAddrAdaOnly n = TxOut (addr n) ada SNothing
+      txOutAddrAdaOnlyDataHash :: Int -> TxOut A
+      txOutAddrAdaOnlyDataHash n = TxOut (addr n) ada (SJust dataHash32)
+      count :: Int
+      count = 10000
    in bgroup
         "TxOut (Alonzo)"
-        [ txOutAlonzoBench "txOutAddr" txOutAddr,
-          txOutAlonzoBench "txOutAddrAdaOnly" txOutAddrAdaOnly,
-          txOutAlonzoBench "txOutAddrAdaonlyDataHash" txOutAddrAdaOnlyDataHash
+        [ txOutAlonzoBench count "txOutAddr" txOutAddr,
+          txOutAlonzoBench count "txOutAddrAdaOnly" txOutAddrAdaOnly,
+          txOutAlonzoBench count "txOutAddrAdaOnlyDataHash" txOutAddrAdaOnlyDataHash
         ]
 
-txOutAlonzoBench :: String -> TxOut A -> Benchmark
-txOutAlonzoBench name txOut' =
-  env (pure txOut') $ \txOut ->
-    bgroup
-      name
-      [ bench "TxOut" $
-          nf
-            (\(TxOut addr vl dh) -> addr `deepseq` vl `deepseq` dh)
-            txOut,
+txOutAlonzoBench :: Int -> String -> (Int -> TxOut A) -> Benchmark
+txOutAlonzoBench count name mkTxOuts =
+  bgroup
+    name
+    [ env (pure (mkTxOuts <$> [1 .. count])) $ \txOuts ->
+        bench "TxOut" $
+          nf (map (\(TxOut addr vl dh) -> addr `deepseq` vl `deepseq` dh)) txOuts,
+      env (pure (mkTxOuts <$> [1 .. count])) $ \txOuts ->
         bench "TxOutCompact" $
           nf
-            ( \case
-                TxOutCompact addr vl -> addr `deepseq` vl
-                TxOutCompactDH addr vl dh -> addr `deepseq` dh `deepseq` vl
+            ( map
+                ( \case
+                    TxOutCompact addr vl -> addr `deepseq` vl
+                    TxOutCompactDH addr vl dh -> addr `deepseq` dh `deepseq` vl
+                )
             )
-            txOut
-      ]
+            txOuts,
+      env (pure (mkTxOuts <$> [1 .. count])) $ \txOuts ->
+        bench "address" $
+          nf
+            (map (getField @"address"))
+            txOuts,
+      env (pure (mkTxOuts <$> [1 .. count])) $ \txOuts ->
+        bench "value" $ nf (map (getField @"value")) txOuts
+    ]
 
-payAddr28 :: KeyHash 'Payment StandardCrypto
-payAddr28 =
-  KeyHash $
-    fromJust $
-      hashFromTextAsHex "0102030405060708090a0b0c0d0e0f12131415161718191a1b1c1d1e"
+payAddr28 :: Int -> KeyHash 'Payment StandardCrypto
+payAddr28 n =
+  let i = n `mod` 10
+      prefix = T.pack (take 6 (cycle (show i)))
+   in KeyHash $
+        fromJust $
+          hashFromTextAsHex
+            (prefix <> "0405060708090a0b0c0d0e0f12131415161718191a1b1c1d1e")
 
 stakeAddr28 :: KeyHash 'Staking StandardCrypto
 stakeAddr28 =
