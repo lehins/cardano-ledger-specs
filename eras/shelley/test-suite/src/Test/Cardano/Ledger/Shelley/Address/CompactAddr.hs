6da48a6918c5fda9dd393a3d1dd36438f9c26413
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2022-04-19 15:25:31 +0300

    Add tests and use faster decompacting method

--- eras/shelley/test-suite/src/Test/Cardano/Ledger/Shelley/Address/CompactAddr.hs
+++ eras/shelley/test-suite/src/Test/Cardano/Ledger/Shelley/Address/CompactAddr.hs
@@ -1,22 +1,30 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Test.Cardano.Ledger.Shelley.Address.CompactAddr where
 
 import Cardano.Ledger.Address (Addr (..), serialiseAddr)
 import qualified Cardano.Ledger.CompactAddress as CA
-import Cardano.Ledger.Credential
-  ( PaymentCredential,
-    StakeReference (..),
-  )
 import qualified Cardano.Ledger.Crypto as CC (Crypto)
+import Data.Bits
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Short as SBS
-import Test.Cardano.Ledger.Shelley.ConcreteCryptoTypes (Mock)
+import Data.Either
+import Data.Maybe (isJust)
 import Test.Cardano.Ledger.Shelley.Serialisation.EraIndepGenerators ()
 import Test.Cardano.Ledger.Shelley.Serialisation.Generators ()
 import Test.QuickCheck
+
+propValidateNewDecompact :: forall crypto. CC.Crypto crypto => Addr crypto -> Property
+propValidateNewDecompact addr =
+  let compact = SBS.toShort $ serialiseAddr addr
+      decompactedOld = CA.deserializeShortAddr @crypto compact
+      decompactedNew = CA.decodeAddrShort @crypto compact
+   in isJust decompactedOld .&&. decompactedOld === decompactedNew
 
 propCompactAddrRoundTrip :: CC.Crypto crypto => Addr crypto -> Bool
 propCompactAddrRoundTrip addr =
@@ -29,41 +37,35 @@ propCompactSerializationAgree addr =
   let (CA.UnsafeCompactAddr sbs) = CA.compactAddr addr
    in sbs == SBS.toShort (serialiseAddr addr)
 
--- Test that we can tell whether this address is a bootstrap address
--- without decoding the rest of the address
-propDecompactAddrLazy :: forall crypto. Mock crypto => Gen Bool
-propDecompactAddrLazy = do
-  addr <- arbitrary :: Gen (Addr crypto)
-  let mangledAddr = CA.decompactAddr . mangle . CA.compactAddr $ addr
-  case mangledAddr of
-    Addr _ _ _ -> pure True
-    AddrBootstrap _ -> pure True
-
--- This tests that reading the keyhash doesn't force the stake reference.
--- Testing that reading the stake reference doesn't force the keyhash in the
--- same way would involve constructing a binary representation of a keyhash with the
--- correct length that wasn't a valid hash, which doesn't seem possible.
-propDecompactShelleyLazyAddr ::
-  forall crypto.
-  CC.Crypto crypto =>
-  Gen Bool
-propDecompactShelleyLazyAddr = do
-  stakeRef <-
+propDecompactErrors :: forall crypto. CC.Crypto crypto => Addr crypto -> Gen Property
+propDecompactErrors addr = do
+  let (CA.UnsafeCompactAddr sbs) = CA.compactAddr addr
+      bs = SBS.fromShort sbs
+      flipHeaderBit b =
+        case BS.uncons bs of
+          Just (h, bsTail) -> BS.cons (complementBit h b) bsTail
+          Nothing -> error "Impossible: CompactAddr can't be empty"
+      mingleHeader = do
+        b <- elements $ case addr of
+          Addr {} -> [1, 2, 3, 7]
+          AddrBootstrap {} -> [0 .. 7]
+        pure ("Header", flipHeaderBit b)
+      mingleAddLength = do
+        NonEmpty xs <- arbitrary
+        pure ("Add Length", bs <> BS.pack xs)
+      mingleDropLength = do
+        n <- chooseInt (1, BS.length bs)
+        pure ("Drop Length", BS.take (BS.length bs - n) bs)
+  (mingler, badAddr) <-
     oneof
-      [ StakeRefBase <$> arbitrary,
-        StakeRefPtr <$> arbitrary
-      ] ::
-      Gen (StakeReference crypto)
-  addr <- Addr <$> arbitrary <*> arbitrary <*> pure stakeRef
-  let keyHash0 = unsafeGetHash addr
-      keyHash1 = unsafeGetHash . CA.decompactAddr . mangle . CA.compactAddr $ addr
-   in pure $ keyHash0 == keyHash1
-
-unsafeGetHash :: Addr crypto -> PaymentCredential crypto
-unsafeGetHash (Addr _ hash _) = hash
-unsafeGetHash _ = error "this can't get a keyhash for a byron address"
-
-mangle :: CA.CompactAddr crypto -> CA.CompactAddr crypto
-mangle (CA.UnsafeCompactAddr bytes) =
-  CA.UnsafeCompactAddr $
-    CA.substring bytes 0 (SBS.length bytes - 1)
+      [ pure ("Empty", ""),
+        mingleHeader,
+        mingleAddLength,
+        mingleDropLength
+        -- minglePayment,
+        -- mingleStaking
+      ]
+  pure $
+    counterexample
+      ("Mingled address with " ++ mingler ++ "was parsed: " ++ show badAddr)
+      $ isLeft $ CA.decodeAddrEither @crypto badAddr
